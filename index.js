16d2e299b9df74debd1a81c32beabf11c8a60c32
MatildaJin <matilda.jin@hujiang.com>
2017-12-14 22:57:09 +0800

    gomoku

---
+++ index.js
@@ -0,0 +1,23 @@
+const Koa = require('koa')
+const path = require('path')
+const nunjucks = require('koa-nunjucks-2')
+const staticFile = require('koa-static')
+
+const app = new Koa()
+const router = require('./router')
+
+app.use(staticFile(path.resolve(__dirname, './public')))
+
+app.use(nunjucks({
+  ext: 'html',
+  path: path.join(__dirname, 'views'),
+  nunjucksConfig: {
+    trimBlocks: true
+  }
+}))
+
+router(app)
+
+app.listen(3000, () => {
+  console.log('Hello World')
+})
